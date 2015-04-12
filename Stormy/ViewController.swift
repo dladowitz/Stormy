e6e221ef2085d0ce54a583200cf165e8fd25952c
David Ladowitz <david@ladowitz.com>
2015-04-12 15:12:50 -0700

    adding forecast.io

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//  https://developer.forecast.io/ reset periodically if left in git repository
+    private let apiKey = "0758afcef711ec228b107286e9686e36"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        let coordinates = "37.8267,-122.423"
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: coordinates, relativeToURL: baseURL)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
