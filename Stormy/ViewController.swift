80fed0d4c625e50206d589c7a639348cc1c05dd8
David Ladowitz <david@ladowitz.com>
2015-04-15 16:25:38 -0700

    connecting refresh button

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var currentTimeLabel: UILabel!
- 
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var percipitationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
 //  https://developer.forecast.io/ reset periodically if left in git repository
     private let apiKey = "0758afcef711ec228b107286e9686e36"
@@ -25,6 +26,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        refreshActivityIndicator.hidden = true
+        getWeatherData()
+    }
+    
+    func getWeatherData(){
         let coordinates = "37.8267,-122.423"
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forecastURL = NSURL(string: coordinates, relativeToURL: baseURL)
@@ -35,10 +41,10 @@ class ViewController: UIViewController {
                 if error == nil {
                     let dataObject = NSData(contentsOfURL: location)
                     let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as! NSDictionary
-//                    println(weatherDictionary)
+                    //                    println(weatherDictionary)
                     
                     let currentWeather = Current(weatherDictionary: weatherDictionary)
-
+                    
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
                         self.temperatureLabel.text = "\(currentWeather.temperature)"
                         self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
@@ -48,18 +54,27 @@ class ViewController: UIViewController {
                         self.summaryLabel.text = currentWeather.summary
                     })
                 }
-
+                
             }
         )
         downloadTask.resume()
+        refreshActivityIndicator.stopAnimating()
+        refreshActivityIndicator.hidden = true
+        refreshButton.hidden = false
         
-
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func refresh() {
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        getWeatherData()
+    }
 
 }
 
